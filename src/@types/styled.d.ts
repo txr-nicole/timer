e4d30b1eb5ed3f8b9dc40a4841a41041ab5c73e1
txr-nicole <nicolefagundes24@gmail.com>
2023-05-08 18:15:23 -0300

    🎉 start project

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import {} from 'styled-components'
+
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType, DefaultTheme {}
+}
