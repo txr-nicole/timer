c2ecbc89419b23eca8d4f332832ec065e0899844
txr-nicole <nicolefagundes24@gmail.com>
2023-05-09 15:06:35 -0300

    🔥 remove Button component

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonComponent, ButtonVariant } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonComponent variant={variant}>Entrar</ButtonComponent>
-}
