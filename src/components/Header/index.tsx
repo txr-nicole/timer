3f423f248a6908808b3b664f8d6d409af3f08fa9
txr-nicole <nicolefagundes24@gmail.com>
2023-05-10 16:14:48 -0300

    ✨ adds a navigation menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
-export function Header( ) {
-    return <h1>Header</h1>
+import { HeaderContainer } from "./styles";
+import { Timer, Scroll } from "phosphor-react";
+
+import logoIgnite from "../../assets/logo-ignite.svg"
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+        <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+            <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+            <Scroll size={24} /> 
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
