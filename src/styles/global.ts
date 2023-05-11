a3e924cf5e937381161557da719d662e14eb43ca
txr-nicole <nicolefagundes24@gmail.com>
2023-05-11 20:46:46 -0300

    📱 add responsive rem

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,12 +8,12 @@ export const GlobalStyle = createGlobalStyle`
 }
 :focus{
     outline:0;
-    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']}
+    box-shadow: 0 0 0 2px ${({ theme }) => theme["green-500"]}
 }
 
 body{
-    background: ${({ theme }) => theme['gray-900']};
-    color:${({ theme }) => theme['gray-300']};
+    background: ${({ theme }) => theme["gray-900"]};
+    color:${({ theme }) => theme["gray-300"]};
     -webkit-flow-smoothing:antialiased;
 }
 body, input-security, textarea, button{
@@ -21,5 +21,12 @@ body, input-security, textarea, button{
     font-weight: 400;
     font-size:1rem;
 }
-
-`
+html{
+        @media (max-width:1080px){
+            font-size: 93.75% // 15px
+        }
+        @media (max-width:720px){
+            font-size:87.5% // 14px
+        }
+    }
+`;
