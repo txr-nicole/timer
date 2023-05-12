cb8366229328ad54b1d8900bed315f756f1323b5
txr-nicole <nicolefagundes24@gmail.com>
2023-05-11 21:03:15 -0300

    💄 creates input and button components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartButton,
+  TaskInput,
+} from "./styles";
 
 export function Home() {
   return (
@@ -7,10 +15,29 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="text" id="task" />
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Descanso" />
+            
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos</span>
         </FormContainer>
@@ -22,10 +49,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button>
-          <Play />
+        <StartButton disabled type="submit">
+          <Play size={24} />
           Começar
-        </button>
+        </StartButton>
       </form>
     </HomeContainer>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,56 +1,119 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.main`
-    flex:1;
+  flex: 1;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-
-    form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 3.5rem;
-    }
+    gap: 3.5rem;
+  }
 `;
 
 export const FormContainer = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: .5rem;
-    color: ${({theme})=>theme["gray-100"]};
-    font-size:1.125rem;
-    font-weight:bold;
-    flex-wrap: wrap;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme["gray-100"]};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+
 `;
 
 export const CountdownContainer = styled.div`
-    font-family: "Roboto Mono", monospace;
-    font-size: 10rem;
-    line-height:8rem;
-    color: ${({theme})=>theme["gray-100"]};
+  font-family: "Roboto Mono", monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${({ theme }) => theme["gray-100"]};
 
-    display:flex;
-    gap: 1rem;
+  display: flex;
+  gap: 1rem;
 
+  span {
+    background: ${({ theme }) => theme["gray-700"]};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+    user-select: none;
+  }
+
+  @media (max-width: 655px) {
+    font-size: 4.8rem;
+    line-height:5rem;
+    gap: 0.5rem;
     span{
-        background: ${({theme})=>theme["gray-700"]};
-        padding: 2rem 1rem;
-        border-radius:8px;
-        user-select: none;
+        padding: 0;
     }
+  }
 `;
 
 export const Separator = styled.div`
-    padding: 2rem 0;
-    color: ${({theme})=>theme["green-500"]};
-    width:4rem;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    user-select:none;
-    `
+  padding: 2rem 0;
+  color: ${({ theme }) => theme["green-500"]};
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  user-select: none;
+
+  @media (max-width: 655px) {
+    padding: 0;
+  }
+`;
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${({ theme }) => theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.09rem;
+  padding: 0 0.5rem;
+  color: ${({ theme }) => theme["gray-100"]};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${({ theme }) => theme["green-500"]};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme["gray-500"]};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+  &::-webkit-calendar-picker-indicator{
+    display: none !important;
+  }
+  
+`;
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
+export const StartButton = styled.button`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.125rem 0;
+  font-size: 1rem;
+  border-radius: 8px;
+  background: ${({ theme }) => theme["green-500"]};
+  border: 0;
+  color: ${({ theme }) => theme["gray-100"]};
+  gap: 0.5rem;
+  transition:.3s all;
+
+  &:hover{
+    background-color: ${({ theme }) => theme["green-700"]}
+  }
+`;
