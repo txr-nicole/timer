e808132ac514d63f6bcaa023683b00e9302435e1
txr-nicole <nicolefagundes24@gmail.com>
2023-05-17 17:04:00 -0300

    💄 creates stopButton

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -99,26 +99,33 @@ export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
 `;
 
-export const StartButton = styled.button`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1.125rem 0;
-  font-size: 1rem;
-  border-radius: 8px;
+ const BaseButton = styled.button`
+   width: 100%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   padding: 1.125rem 0;
+   font-size: 1rem;
+   border-radius: 8px;
+   border: 0;
+   gap: 0.5rem;
+   transition: 0.3s all;
+   color: ${({ theme }) => theme["gray-100"]};
+ `;
+
+export const StartButton = styled(BaseButton)`
   background: ${({ theme }) => theme["green-500"]};
-  border: 0;
   color: ${({ theme }) => theme.white};
-  gap: 0.5rem;
-  transition:.3s all;
 
-  &:hover{
-    background-color: ${({ theme }) => theme["green-700"]}
-  }
-
-  &:disabled{
+  &:not(:disabled):hover {
     background-color: ${({ theme }) => theme["green-700"]};
-    color: ${({ theme }) => theme["gray-100"]};
   }
-`;
+
+  
+`; 
+export const StopButton = styled(BaseButton)`
+  background: ${({ theme }) => theme["red-500"]};
+  &:not(:disabled):hover {
+    background-color: ${({ theme }) => theme["red-700"]};
+  }
+`; 
